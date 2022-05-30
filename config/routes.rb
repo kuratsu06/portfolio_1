09e1082c0d832a06ecf627e1eea0867bdf23d191
yuna <yuna@MacBook-Air.local>
2022-05-31 01:32:18 +0900

    [add]トップページの作成
    header/footerの共通化、homeのcontroller、viewを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  scope :portfolio_1 do
+    root 'home#top'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
